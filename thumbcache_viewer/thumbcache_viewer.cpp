cedb6a8ddac98ab417005955af3e7bd6745acd7d
defeatthefreak@gmail.com <defeatthefreak@gmail.com@08c1b10c-105d-5d1c-da51-fa80f18cc9b0>
2011-04-16 22:18:47 +0000

    Initial release of GUI code.

---
+++ thumbcache_viewer/thumbcache_viewer.cpp
@@ -0,0 +1,117 @@
+/*
+    thumbcache_viewer will extract thumbnail images from thumbcache database files.
+    Copyright (C) 2011  Eric Kutcher
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "globals.h"
+
+// We want to get these objects before the window is shown.
+
+// Object variables
+HWND g_hWnd_main = NULL;	// Handle to our main window.
+HWND g_hWnd_image = NULL;	// Handle to the image window.
+
+HFONT hFont = NULL;			// Handle to our font object.
+
+HICON hIcon_bmp = NULL;		// Handle to the system's .bmp icon.
+HICON hIcon_jpg = NULL;		// Handle to the system's .jpg icon.
+HICON hIcon_png = NULL;		// Handle to the system's .png icon.
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
+{
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+   
+	// Initialize GDI+.
+	Gdiplus::GdiplusStartup( &gdiplusToken, &gdiplusStartupInput, NULL );
+
+	// Get the default message system font.
+	NONCLIENTMETRICS ncm = { NULL };
+	ncm.cbSize = sizeof( NONCLIENTMETRICS );
+	SystemParametersInfoW( SPI_GETNONCLIENTMETRICS, sizeof( NONCLIENTMETRICS ), &ncm, 0 );
+	
+	// Set our global font to the LOGFONT value obtained from the system.
+	hFont = CreateFontIndirect( &ncm.lfMessageFont );
+
+	// Get the system icon for each of the three file types.
+	SHFILEINFO shfi = { NULL }; 
+	SHGetFileInfo( L".bmp", FILE_ATTRIBUTE_NORMAL, &shfi, sizeof( shfi ), SHGFI_ICON | SHGFI_SMALLICON | SHGFI_USEFILEATTRIBUTES );
+	hIcon_bmp = shfi.hIcon;
+	SHGetFileInfo( L".png", FILE_ATTRIBUTE_NORMAL, &shfi, sizeof( shfi ), SHGFI_ICON | SHGFI_SMALLICON | SHGFI_USEFILEATTRIBUTES );
+	hIcon_png = shfi.hIcon;
+	SHGetFileInfo( L".jpg", FILE_ATTRIBUTE_NORMAL, &shfi, sizeof( shfi ), SHGFI_ICON | SHGFI_SMALLICON | SHGFI_USEFILEATTRIBUTES );
+	hIcon_jpg = shfi.hIcon;
+
+	// Initialize our window class.
+	WNDCLASSEX wcex;
+	wcex.cbSize			= sizeof( WNDCLASSEX );
+	wcex.style          = CS_VREDRAW | CS_HREDRAW;
+	wcex.lpfnWndProc    = MainWndProc;
+	wcex.cbClsExtra     = 0;
+	wcex.cbWndExtra     = 0;
+	wcex.hInstance      = hInstance;
+	wcex.hIcon          = LoadIcon( hInstance, MAKEINTRESOURCE( IDI_ICON ) );
+	wcex.hCursor        = LoadCursor( NULL, IDC_ARROW );
+	wcex.hbrBackground  = ( HBRUSH )( COLOR_WINDOW );
+	wcex.lpszMenuName   = NULL;
+	wcex.lpszClassName  = L"thumbcache";
+	wcex.hIconSm        = LoadIcon( wcex.hInstance, MAKEINTRESOURCE( IDI_APPLICATION ) );
+
+	if ( !RegisterClassEx( &wcex ) )
+	{
+		MessageBox( NULL, L"Call to RegisterClassEx failed!", PROGRAM_CAPTION, MB_ICONWARNING );
+		return 1;
+	}
+
+	wcex.lpfnWndProc    = ImageWndProc;
+	wcex.lpszClassName  = L"image";
+
+	if ( !RegisterClassEx( &wcex ) )
+	{
+		MessageBox( NULL, L"Call to RegisterClassEx failed!", PROGRAM_CAPTION, MB_ICONWARNING );
+		return 1;
+	}
+
+	g_hWnd_main = CreateWindow( L"thumbcache", PROGRAM_CAPTION, WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, ( ( GetSystemMetrics( SM_CXSCREEN ) - MIN_WIDTH ) / 2 ), ( ( GetSystemMetrics( SM_CYSCREEN ) - MIN_HEIGHT ) / 2 ), MIN_WIDTH, MIN_HEIGHT, NULL, NULL, NULL, NULL );
+
+	if ( !g_hWnd_main )
+	{
+		MessageBox( NULL, L"Call to CreateWindow failed!", PROGRAM_CAPTION, MB_ICONWARNING );
+		return 1;
+	}
+
+	g_hWnd_image = CreateWindow( L"image", PROGRAM_CAPTION, WS_OVERLAPPEDWINDOW, ( ( GetSystemMetrics( SM_CXSCREEN ) - MIN_WIDTH ) / 2 ), ( ( GetSystemMetrics( SM_CYSCREEN ) - MIN_HEIGHT ) / 2 ), MIN_HEIGHT, MIN_HEIGHT, NULL, NULL, NULL, NULL );
+
+	if ( !g_hWnd_image )
+	{
+		MessageBox( NULL, L"Call to CreateWindow failed!", PROGRAM_CAPTION, MB_ICONWARNING );
+		return 1;
+	}
+
+	ShowWindow( g_hWnd_main, SW_SHOW );
+
+	// Main message loop:
+	MSG msg;
+	while ( GetMessage( &msg, NULL, 0, 0 ) > 0 )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	// Shutdown GDI+
+	Gdiplus::GdiplusShutdown( gdiplusToken );
+
+	return ( int )msg.wParam;
+}
